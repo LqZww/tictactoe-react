096574a8ef5d6733aff292fb341251c9abcbec0a
ZWW <951598077@qq.com>
2019-11-19 23:09:48 +0800

    Updates

---
+++ precache-manifest.30ce2d5a4060fb9735f00a59876b2907.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a0caee38f4f64ec25c4dc4422b181b",
+    "url": "/tictactoe-react/index.html"
+  },
+  {
+    "revision": "79918e059a4a9a556e76",
+    "url": "/tictactoe-react/static/css/main.e47b33a4.chunk.css"
+  },
+  {
+    "revision": "58eb719f50b361e66f3b",
+    "url": "/tictactoe-react/static/js/2.5b5a15a3.chunk.js"
+  },
+  {
+    "revision": "79918e059a4a9a556e76",
+    "url": "/tictactoe-react/static/js/main.26657459.chunk.js"
+  },
+  {
+    "revision": "fb12c47aa30f5efc055a",
+    "url": "/tictactoe-react/static/js/runtime-main.bc126ad5.js"
+  }
+]);
